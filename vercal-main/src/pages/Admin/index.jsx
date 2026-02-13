81a99d1a0f16d649fc8eca34a74f3e5a7e01a9c3
preciouslizzie <oryaelizabeth98@gmail.com>
2026-02-13 09:02:29 +0100

    1302

--- vercal-main/src/pages/Admin/index.jsx
+++
@@ -1,7 +0,0 @@
-export { default as AdminDashboard } from './AdminDashboard';
-export { default as AdminVolunteers } from './AdminVolunteers';
-export { default as AdminRoles } from './AdminRoles';
-export { default as AdminSchedules } from './AdminSchedules';
-export { default as AdminAnnouncements } from './AdminAnnouncements';
-export { default as AdminGroups } from './AdminGroups';
-export { default as AdminReports } from './AdminReports';
